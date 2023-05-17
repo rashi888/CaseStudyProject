c076127d8be962226d0f989748dab130a8f833d8
Srijan <srijanksah02@gmail.com>
2023-05-17 21:32:03 +0530

    login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../assets/logo.png'
-import { FaUser } from 'react-icons/fa';
-import { FaShoppingBag } from 'react-icons/fa'
-import '../styles/Navbar.css';
-import { BiSearch } from 'react-icons/bi'
-// import Navy from './Navy';
+import React from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { BiSearch } from "react-icons/bi";
+import { FaUser } from "react-icons/fa";
+import { FaShoppingBag } from "react-icons/fa";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
-    return (
-        <>
-            <div className="header navbar px-5 " >
-                <div className="logo">
-                    <Link to="/home">  <img src={logo} alt="" className='logo' /></Link>
-                </div>
-                <div className="searchBar" >
-                    <input class="inputSearch form-control mr-sm-2" type="search" placeholder="Items to Search..." aria-label="Search" />
-                    <div className="searchIcon"><BiSearch /></div>
-                </div>
+  return (
+    <div>
+        
+      <div className="header navbar  ">
+        <div className="logo">
 
-                <div className="icons" >
-                    <Link to="/login">  <div className="login text-center" id='login' ><FaUser /></div></Link>
-                    <Link to="/cart"> <div className="cart" id="cart" ><FaShoppingBag /></div></Link>
-                </div>
+          <Link to="/"> 
+          <img src={logo} alt="" className="logo" />
+          </Link>
+        </div>
+        <div className="searchBar">
+          <input
+            className="inputSearch form-control mr-sm-2"
+            type="search"
+            placeholder="Items to Search..."
+            aria-label="Search"
+          />
+          <div className="searchIcon">
+            <BiSearch />
+          </div>
+        </div>
+
+        <div className="icons">
+          <Link to="/login">
+            {" "}
+            <div className="login text-center" id="login">
+              <FaUser />
             </div>
-
-        </>
-    );
-}
+          </Link>
+          <Link to="/">
+            {" "}
+            <div className="cart" id="cart">
+              <FaShoppingBag />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
