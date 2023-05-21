65631347039373fccdbffd78a99593853b03d459
Srijan <srijanksah02@gmail.com>
2023-05-21 09:42:14 +0530

    product

--- src/pages/ProductsAdd.jsx
+++ src/pages/ProductsAdd.jsx
@@ -1,31 +1,40 @@
 import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 
 
 function ProductsAdd() {
-
-    const[productName,setProductName]=useState("");
-    const[productDescription,setProductDescription]=useState("");
-    const[productPhoto,setProductPhoto]=useState("");
-    const[productPrice,setProductPrice]=useState("");
-    const[categoryId,setCategoryId]=useState("");
     const [category, setCategory] = useState([]);
+    const [categoryId, setCategoryId] = useState("");
 
-    async function addProduct(){
-        console.log(productName,productDescription,productPhoto,productPrice,categoryId);
-        const formData=new FormData();
-        formData.append('productPhoto',productPhoto);
-        formData.append('productPrice',productPrice);
-        formData.append('productName',productName);
-        formData.append('productDescription',productDescription);
-        formData.append('categoryId',categoryId);
-        console.log(formData);
-        let result=await fetch("http://localhost:8080/api/category/product"+categoryId,{
-            method:'POST',
-            body:formData
-        });
-        alert("Data has been saved")
-    }
+    const [data, setData] = useState({
+        productName: "",
+        productPrice: "",
+        productDescription: "",
+        productPhoto: "",
+    });
+    
+  
+    useEffect(() => {
+      console.log(data);
+    }, [data]);
+  
+    const handleChange = (event, property) => {
+      setData({ ...data, [property]: event.target.value });
+    };
+      
+    const submitForm = (event) => {
+      event.preventDefault();
+        axios.post("http://localhost:8080/api/category/product/"+categoryId,data)
+        .then((resp) => {
+            console.log(resp);
+            console.log("success log");
+            alert("Product added successfully!");
+            })
+            .catch((error) => {
+            console.log(error);
 
+            });
+    };
     
 
 
@@ -44,20 +53,21 @@ function ProductsAdd() {
         <>
             <div className="containerr" style={{margin:'3% 7%',border:'0.3px solid lightgray',padding:'2%'}}>
                 <h3>Add a new Product</h3>
-                <form enctype="multipart/form-data">
+                <form onSubmit={submitForm}>
                     <div className="row">
                         <div className="col-sm-5">
 
                             <input type="hidden" name="id" />
                             <div className="form-group">
-                                <label for="ProductName">Name</label>
-                                <input type="text" className="form-control" onChange={(e)=>setProductName(e.target.value)} required name="productName" id="productName"
+                                <label for="productName">Name</label>
+                                <input type="text" className="form-control"  onChange={(e) => handleChange(e, "productName")}
+                  value={data.productName} required name="productName" id="productName"
                                     placeholder="Enter name" />
 
                             </div>
                             <div className="form-group">
                                 <label for="category">Select Category</label>
-                                <select className="form-control" onChange={(e)=>setCategoryId(e.target.value)} id="category" name="category">
+                                <select className="form-control" onChange={(e)=>setCategoryId(e.target.value)} id="categoryId" name="categoryId">
                                     <option value="">Select Category</option>
                                     {category.map((item) => {
                                         return (
@@ -67,27 +77,28 @@ function ProductsAdd() {
                                 </select>
                             </div>
                             <div className="form-group">
-                                <label for="price">Price</label>
-                                <input type="number" className="form-control"  onChange={(e)=>setProductPrice(e.target.value)} required name="productPrice" id="productPrice"
+                                <label for="productPrice">Price</label>
+                                <input type="number" className="form-control"   onChange={(e) => handleChange(e, "productPrice")}
+                  value={data.productPrice} required name="productPrice" id="productPrice"
                                     placeholder="Price" />
                             </div>
                             
                             <div className="form-group">
                                 <label for="productDescription">Product Description</label>
-                                <textarea className="form-control" onChange={(e)=>setProductDescription(e.target.value)} rows="5" name="productDescription" id="productDescription"></textarea>
+                                <textarea className="form-control"  onChange={(e) => handleChange(e, "productDescription")}
+                  value={data.productDescription} rows="5" name="productDescription" id="productDescription"></textarea>
                             </div>
                         </div>
                         <div className="col-sm-5">
                             <p>Product Image</p>
-                            <div className="custom-file">
+                            {/* <div className="custom-file">
                                 <input type="file" className="custom-file-input" onChange={(e)=>setProductPhoto(e.target.files[0])} name="productPhoto" accept="image/jpeg, image/png" id="productPhoto" />
                                 <label className="custom-file-label" for="productImage">Choose file</label>
-                            </div>
-                            <div className="form-group">
-                                <img src="#" id="imgPreview" height="100px" width="100px" style={{marginTop: "20px"}} alt=" " />
-                            </div>
-                            <input type="hidden" name="imgName"  />
-                            <button onClick={addProduct} type="submit" className="btn btn-primary">Add product</button>
+                            </div> */}
+                            <input type="text"  onChange={(e) => handleChange(e, "productPhoto")}
+                  value={data.productPhoto} name="productPhoto" id="" />
+                            
+                            <button type="submit" className="btn btn-primary">Add product</button>
                         </div>
                     </div>
                 </form>
