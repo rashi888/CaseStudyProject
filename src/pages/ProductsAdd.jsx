31aa2bedc53475103d5b78f6ce6c8d3df7ad4103
Srijan <srijanksah02@gmail.com>
2023-05-21 07:37:57 +0530

    product

--- src/pages/ProductsAdd.jsx
+++ src/pages/ProductsAdd.jsx
@@ -1,27 +1,44 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 
 
 function ProductsAdd() {
 
-    const[name,setName]=useState("");
-    const[description,setDescription]=useState("");
-    const[file,setFile]=useState("");
-    const[price,setPrice]=useState("");
-    const[category,setCategory]=useState("");
+    const[productName,setProductName]=useState("");
+    const[productDescription,setProductDescription]=useState("");
+    const[productPhoto,setProductPhoto]=useState("");
+    const[productPrice,setProductPrice]=useState("");
+    const[categoryId,setCategoryId]=useState("");
+    const [category, setCategory] = useState([]);
 
     async function addProduct(){
-        console.log(name,description,file,price);
+        console.log(productName,productDescription,productPhoto,productPrice,categoryId);
         const formData=new FormData();
-        formData.append('file',file);
-        formData.append('price',price);
-        formData.append('name',name);
-        formData.append('description',description);
-        let result=await fetch("http://localhost:8080/api/category/categoryId/product",{
+        formData.append('productPhoto',productPhoto);
+        formData.append('productPrice',productPrice);
+        formData.append('productName',productName);
+        formData.append('productDescription',productDescription);
+        formData.append('categoryId',categoryId);
+        console.log(formData);
+        let result=await fetch("http://localhost:8080/api/category/product"+categoryId,{
             method:'POST',
             body:formData
         });
         alert("Data has been saved")
     }
+
+    
+
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8080/api/categories/");
+    const data = await response.json();
+    return setCategory(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(category);
 
     return (
         <>
@@ -33,32 +50,37 @@ function ProductsAdd() {
 
                             <input type="hidden" name="id" />
                             <div className="form-group">
-                                <label for="name">Name</label>
-                                <input type="text" className="form-control" onChange={(e)=>setName(e.target.value)} required name="name" id="name"
+                                <label for="ProductName">Name</label>
+                                <input type="text" className="form-control" onChange={(e)=>setProductName(e.target.value)} required name="productName" id="productName"
                                     placeholder="Enter name" />
 
                             </div>
                             <div className="form-group">
                                 <label for="category">Select Category</label>
-                                <select className="form-control" onChange={(e)=>setName(e.target.value)} id="category" name="category">
-                                    <option />
+                                <select className="form-control" onChange={(e)=>setCategoryId(e.target.value)} id="category" name="category">
+                                    <option value="">Select Category</option>
+                                    {category.map((item) => {
+                                        return (
+                                            <option value={item.categoryId}>{item.categoryTitle}</option>
+                                        );
+                                    })}
                                 </select>
                             </div>
                             <div className="form-group">
                                 <label for="price">Price</label>
-                                <input type="number" className="form-control"  onChange={(e)=>setPrice(e.target.value)} required name="price" id="price"
+                                <input type="number" className="form-control"  onChange={(e)=>setProductPrice(e.target.value)} required name="productPrice" id="productPrice"
                                     placeholder="Price" />
                             </div>
                             
                             <div className="form-group">
-                                <label for="description">Product Description</label>
-                                <textarea className="form-control" onChange={(e)=>setDescription(e.target.value)} rows="5" name="description" id="description"></textarea>
+                                <label for="productDescription">Product Description</label>
+                                <textarea className="form-control" onChange={(e)=>setProductDescription(e.target.value)} rows="5" name="productDescription" id="productDescription"></textarea>
                             </div>
                         </div>
                         <div className="col-sm-5">
                             <p>Product Image</p>
                             <div className="custom-file">
-                                <input type="file" className="custom-file-input" onChange={(e)=>setFile(e.target.files[0])} name="productImage" accept="image/jpeg, image/png" id="productImage" />
+                                <input type="file" className="custom-file-input" onChange={(e)=>setProductPhoto(e.target.files[0])} name="productPhoto" accept="image/jpeg, image/png" id="productPhoto" />
                                 <label className="custom-file-label" for="productImage">Choose file</label>
                             </div>
                             <div className="form-group">
