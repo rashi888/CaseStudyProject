2887d6d1efed5345492936fa3ffaf68c0506a173
Srijan <srijanksah02@gmail.com>
2023-05-23 20:23:07 +0530

    pic

--- src/components/Navy/Navy.jsx
+++ src/components/Navy/Navy.jsx
@@ -16,13 +16,11 @@ function Navy() {
             <div>
 
                 <link to="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet" />
-                {/* call to action */}
-                {/* nav navigation commerce */}
                 <div className="nav-container" style={{ marginTop: '10%', marginBottom: '-2%', zIndex: 17 }}>
                     <nav className="all-category-nav">
                         <label className="open-menu-all" htmlFor="open-menu-all">
-                            <input className="input-menu-all" id="open-menu-all" type="checkbox" name="menu-open" />
-                            <span className="all-navigator" style={{ textDecoration: 'none' }}><i className="fas fa-bars" /> <span>All category</span> <i className="fas fa-angle-down" />
+                            <input  className="input-menu-all" id="open-menu-all" type="checkbox" name="menu-open" />
+                            <span className="all-navigator" style={{ textDecoration: 'none' }}><i className="fas fa-bars" /> 
                                 <i className="fas fa-angle-up" />
                             </span>
                             <div className="category1">
@@ -33,14 +31,7 @@ function Navy() {
                             <ul className="all-category-list">
                                 <li className="all-category-list-item" ><Link to="/mobiles" className="all-category-list-link" style={{ textDecoration: 'none', color: 'gray' }}>Smartphones<i className="fas fa-angle-right" /></Link>
                                     <div className="category-second-list">
-                                        <ul className="category-second-list-ul">
-                                            <li className="category-second-item"><Link to="">Iphone 10</Link></li>
-                                            <li className="category-second-item"><Link to="">Galaxy Note 10</Link></li>
-                                            <li className="category-second-item"><Link to="">Motorola One </Link></li>
-                                            <li className="category-second-item"><Link to="">Galaxy A80 </Link></li>
-                                            <li className="category-second-item"><Link to="">Galaxy M </Link></li>
-                                            <li className="category-second-item"><Link to="">Huaway P30 </Link></li>
-                                        </ul>
+                                        
                                     </div>
                                 </li>
                                 <li className="all-category-list-item"><Link to="" className="all-category-list-link">Furniture <i className="fas fa-angle-right" /></Link></li>
