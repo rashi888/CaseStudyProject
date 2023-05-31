2977503918c1aa850068b85875c6dd75f2287569
Srijan <srijanksah02@gmail.com>
2023-05-31 16:07:36 +0530

    order

--- src/components/OrderHistory/OrderHistory.jsx
+++ src/components/OrderHistory/OrderHistory.jsx
@@ -24,38 +24,37 @@ function OrderHistory() {
         <>
             <div class="container-fluid">
                 <h1 >Order History</h1>
-                <a style={{ margin: '20px 0px', color: 'white', alignItems: 'right', marginLeft: '80%' }} class="btn btn-primary">Place New Order</a>
+                <Link to="/" style={{ margin: '20px 0px', color: 'white', alignItems: 'right', marginLeft: '80%' }} class="btn btn-primary">Place New Order</Link>
                 <table class="table" style={{backgroundColor:'white'}}>
                     <thead class="thead-light">
                         <tr>
                             <th scope="col">Order Id.</th>
                             <th scope="col">Order Date</th>
-                            <th scope="col">Order Status</th>
+                            <th scope="col">Shipping Address</th>
                             <th scope="col">Order Method</th>
                             <th scope="col">Price</th>
                             <th scope="col">Status</th>
                         </tr>
                     </thead>
+
+                    {order.map((item, index) => {
+
+                        return (
+                            <>
                     <tbody>
                         <tr >
-                            <th scope="row" >19865430</th>
-                            <td >29/05/2023</td>
-                            <td>Tata Pvt.</td>
-                            <td>User Name</td>
-                            <td >45,000</td>
-                            <td > <span style={{ backgroundColor: 'green', padding: '8px', borderRadius: '5px', color: 'white' }}>Completed</span> </td>
+                            <th scope="row" >{item.orderId}</th>
+                            <td >{item.orderDate}</td>
+                            <td>{item.shippingAddress}</td>
+                            <td>{item.paymentMethod}</td>
+                            <td >{item.totalPrice}</td>
+                            <td > <span style={{ backgroundColor: 'green', padding: '8px', borderRadius: '5px', color: 'white' }}>{item.orderStatus}</span> </td>
                         </tr>
                     </tbody>
-                    <tbody>
-                        <tr >
-                            <th scope="row" >19865430</th>
-                            <td >29/05/2023</td>
-                            <td>Tata Pvt.</td>
-                            <td>User Name</td>
-                            <td >45,000</td>
-                            <td > <span style={{ backgroundColor: 'green', padding: '8px', borderRadius: '5px', color: 'white' }}>Completed</span> </td>
-                        </tr>
-                    </tbody>
+                            </>
+                        );
+                    })}
+                   
                 </table>
             </div>
 
