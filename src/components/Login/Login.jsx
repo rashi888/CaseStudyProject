54a9f00b48b2e9bcaff8a8a2e580edd209edb15a
Srijan <srijanksah02@gmail.com>
2023-06-06 13:41:57 +0530

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -10,8 +10,75 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import Swal from "sweetalert2";
-
+import GitHubLogin from 'react-github-login';
 const Login = () => {
+
+  const onSuccess = (response) => console.log(response);
+
+  const onFailure = (response) => console.error(response);
+  
+
+
+
+
+  const handleCredentialResponse = (response) => {
+    console.log(response.credential);
+    const token = response.credential;
+    const decoded = jwt_decode(token);
+    // console.log(decoded);
+    const data = {
+      name: decoded.name,
+      emailId: decoded.email,
+
+    };
+    console.log(data);
+
+    axios.post("http://localhost:8080/api/users/OAuth", data).then((response) => {
+      console.log(response.data);
+      window.localStorage.setItem("token", response.data.token);
+      window.localStorage.setItem("userId", response.data.userId);
+      window.localStorage.setItem("role", response.data.role);
+      window.localStorage.setItem("name", response.data.name);
+      navigate("/");
+    }
+    ).catch((error) => {
+      const errorMsg = error.response.data.message;
+      if (errorMsg.includes("Email"))
+        toast.error("Email already exists!", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+        });
+
+
+    });
+  }
+
+  useEffect(() => {
+    const script = document.createElement('script');
+        script.src = 'https://accounts.google.com/gsi/client';
+        script.async = true;
+    google.accounts.id.initialize({
+      client_id: '889458596682-oujv8pjnafnps4o28rdii03t9bhtn5qq.apps.googleusercontent.com',
+      callback: handleCredentialResponse,
+      auto_select: true,
+      cancel_on_tap_outside: false
+    });
+
+
+    google.accounts.id.renderButton(
+      document.getElementById("singInDiv"),
+      {
+        theme: "outline",
+        size: "large",
+        text: "Continue with Google",
+        shape: "circle",
+      }
+    );
+  }, [])
+
 
 
 
@@ -126,6 +193,15 @@ const Login = () => {
             <div className="field">
               <input type="submit" value="Login" />
             </div>
+            <div className="google-btn" id="singInDiv"></div>
+            <div className="github-btn">
+              <GitHubLogin clientId="b2d3426448bdd9f60237"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                redirectUri=""
+              />
+            </div>
+
             <div className="signup-link">
               Not a member? <a href="signup">Signup now</a>
             </div>
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -20,65 +20,7 @@ import Swal from "sweetalert2";
 
 const Signup = () => {
 
-  const handleCredentialResponse = (response) => {
-    console.log(response.credential);
-    const token = response.credential;
-    const decoded = jwt_decode(token);
-    // console.log(decoded);
-    const data = {
-      name: decoded.name,
-      emailId: decoded.email,
-
-    };
-    console.log(data);
-
-    axios.post("http://localhost:8080/api/users/OAuth", data).then((response) => {
-      console.log(response.data);
-      window.localStorage.setItem("token", response.data.token);
-      window.localStorage.setItem("userId", response.data.userId);
-      window.localStorage.setItem("role", response.data.role);
-      window.localStorage.setItem("name", response.data.name);
-      navigate("/");
-    }
-    ).catch((error) => {
-      const errorMsg = error.response.data.message;
-      if (errorMsg.includes("Email"))
-        toast.error("Email already exists!", {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-        });
-
-
-    });
-  }
-
-  useEffect(() => {
-    const script = document.createElement('script');
-        script.src = 'https://accounts.google.com/gsi/client';
-        script.async = true;
-    google.accounts.id.initialize({
-      client_id: '889458596682-oujv8pjnafnps4o28rdii03t9bhtn5qq.apps.googleusercontent.com',
-      callback: handleCredentialResponse,
-      auto_select: true,
-      cancel_on_tap_outside: false
-    });
-
-
-    google.accounts.id.renderButton(
-      document.getElementById("singInDiv"),
-      {
-        theme: "outline",
-        size: "large",
-        text: "signIn",
-        shape: "rectangular",
-        logo_alignment: "left"
-      }
-    );
-  }, [])
-
+  
 
   const [data, setData] = useState({
     name: "",
@@ -249,7 +191,6 @@ const Signup = () => {
               <input type="submit" value="Register" />
             </div>
 
-            <div className="google-btn" id="singInDiv"></div>
 
             <div className="signup-link">
               Already a member? <a href="login">Login</a>
