8ed9187fd6abbe743d52da1db3beee8fa322a8b6
Srijan <srijanksah02@gmail.com>
2023-06-07 12:36:49 +0530

    merchant

--- src/pages/Admin/MerchantList.jsx
+++ src/pages/Admin/MerchantList.jsx
@@ -2,9 +2,57 @@ import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import b1 from "../../assets/PhoneImgs/phone1.webp"
 import Swal from 'sweetalert2';
+import axios from 'axios';
 
 function MerchantList() {
 
+    const [user, setUser] = useState([]);
+
+    const fetchData = () => {
+        return fetch("http://localhost:8080/api/users/")
+            .then((response) => response.json())
+            .then((data) => setUser(data));
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    console.log(user);
+
+    const deleteuser = (userId) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You will not be able to recover this User!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'No, keep it',
+
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete("http://localhost:8080/api/users/" + userId)
+
+
+                    .then((resp) => {
+                        console.log(resp["data"]);
+                        console.log("success log");
+                        Swal.fire({
+                            title: "Success",
+                            text: "User Deleted Successfully",
+                            icon: "success",
+                        });
+                        window.location.reload();
+                    })
+            } else if (result.dismiss === Swal.DismissReason.cancel) {
+                Swal.fire(
+                    'Cancelled',
+                    'User is safe :)',
+                    'error'
+                )
+            }
+        })
+    }
     
 
 
@@ -27,12 +75,13 @@ function MerchantList() {
                         </tr>
                     </thead>
                     {user.map((item, index) => {
+                        if (item.role === "MERCHANT")
                         return (<>
                             <tbody>
                                 <tr >
                                     <th scope="row" style={{ width: '120px' }} key={item.userId}> {index + 1}</th>
-                                    <td style={{ width: '530px' }}>{item.userName}</td>
-                                    <td >{item.mobile}</td>
+                                    <td style={{ width: '530px' }}>{item.name}</td>
+                                    <td >{item.mobileNumber}</td>
                                     <td>₹ {item.emailId}</td>
                                     <td><button className="btnn btn-danger" style={{ padding: '4px 7px', borderRadius: '5px', marginTop: '-7px' }} onClick={() => deleteuser(item.userId)}>Delete</button></td>
                                     <td><Link to={'/updateuser/' + item.userId} className="btnn btn-warning" style={{ padding: '4px 7px', borderRadius: '5px', marginTop: '-7px' }}>Update</Link></td>
