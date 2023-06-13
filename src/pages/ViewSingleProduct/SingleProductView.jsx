cbd79b6a7ccf34aa275663e6b7ffb04390f6aa0e
rashi <rashidixit129@gmail.com>
2023-06-13 23:41:29 +0530

    .

--- src/pages/ViewSingleProduct/SingleProductView.jsx
+++ src/pages/ViewSingleProduct/SingleProductView.jsx
@@ -84,7 +84,7 @@ function SingleProductView() {
                 data.productPhoto.map((item) => {
                   return (
                     <div className="image-group">
-                      <img  src={"http://localhost:8080/api/products/image/" + item} alt="" height="90%" width="90%"/>
+                      <img  src={"http://localhost:8080/api/products/image/" + item} alt="" height="100px" />
                     </div>
                   )
                 }
