6a1cf737378464b4391fcb956f751dccb248e823
Srijan <srijanksah02@gmail.com>
2023-06-13 23:48:57 +0530

    pro

--- src/pages/ViewSingleProduct/SingleProductView.jsx
+++ src/pages/ViewSingleProduct/SingleProductView.jsx
@@ -4,11 +4,9 @@ import { Link } from 'react-router-dom';
 import Swal from "sweetalert2";
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-
+import singleprod from "../../assets/MoreImgs/shopease single product view2.png"
 
 function SingleProductView() {
-
-  const [image, setImage] = useState(null);
 
   const { id } = useParams();
   const productId = id;
@@ -86,7 +84,7 @@ function SingleProductView() {
                 data.productPhoto.map((item) => {
                   return (
                     <div className="image-group">
-                      <img height="100px" src={"http://localhost:8080/api/products/image/" + item} onClick={setImage("http://localhost:8080/api/products/image/" + item)} alt="" />
+                      <img  src={"http://localhost:8080/api/products/image/" + item} alt="" height="100px" />
                     </div>
                   )
                 }
@@ -94,21 +92,21 @@ function SingleProductView() {
               }
             </div>
             <div className="right-of-left">
-              <img setImage={"http://localhost:8080/api/products/image/" + data.productPhoto[0]} src={image} height="80%" width="80%" />
+              <img src={"http://localhost:8080/api/products/image/" + data.productPhoto[0]} height="100%" width="100%" />
             </div>
           </div>
           <div className="right-for-content">
 
             <h2>{data.productName}</h2>
-            <h6 style={{ marginTop: '15px', textAlign: 'start' }}>Extra {data.productMRP - data.productPrice} off</h6>
+            <h6 style={{ marginTop: '15px', textAlign: 'start' }}>Extra ₹{data.productMRP - data.productPrice} off</h6>
             <div className="pp" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
 
-              <h1>₹{data.productPrice} <h6><del>{data.productMRP}</del> <span style={{ color: 'darkgreen' }}>{(((data.productMRP - data.productPrice) * 100) / data.productMRP).toFixed(2)}%</span> </h6></h1>
+              <h1 style={{}}>₹{data.productPrice} <h6>M.R.P. <del>₹{data.productMRP}</del> <span style={{ color: 'darkgreen' }}>{(((data.productMRP - data.productPrice) * 100) / data.productMRP).toFixed(2)}% off</span> </h6></h1>
             </div>
             {/* <hr style={{ border: '1px solid gray' }} />
             <h6 style={{ fontWeight: 400 }}>Description</h6>
             <div dangerouslySetInnerHTML={{ __html: data.productDescription }}></div> */}
-
+            <img src={singleprod} height="40%" width="70%" style={{display:'flex',alignItems:'center'}}/>
             <div className="btn-add-div" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', marginLeft: '-30px' }}>
               <Link onClick={addtocart} to="/"><button style={{ backgroundColor: '#33bbca', borderRadius: '5px', fontSize: '20px' }}>Add to cart</button></Link>
               <Link> <button style={{ backgroundColor: '#33bbca', borderRadius: '5px', fontSize: '20px' }}>Buy Now</button> </Link>
