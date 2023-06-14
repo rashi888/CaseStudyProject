01d3066f024862bd7026e5a01faba48276cad632
Srijan <srijanksah02@gmail.com>
2023-06-14 12:42:20 +0530

    admin

--- src/pages/Admin/ProductListAdmin.jsx
+++ src/pages/Admin/ProductListAdmin.jsx
@@ -3,26 +3,43 @@ import { Link } from "react-router-dom";
 import b1 from "../../assets/PhoneImgs/phone1.webp"
 import Swal from 'sweetalert2';
 import { Spinner } from 'reactstrap'
+import axios from 'axios'
 
 function ProductListAdmin() {
 
     const [isLoading, setIsLoading] = useState(false);
 
     const deleteProduct = (id) => {
-        fetch("http://localhost:8080/api/products/" + id, {
-            method: "DELETE",
+        axios.delete("http://localhost:8080/api/products/" + id, {
             headers: {
                 "Authorization": "Bearer " + localStorage.getItem("token"),
             }
-        }).then(() => {
-            Swal.fire({
-                title: "Success",
-                text: "Product Deleted Successfully",
-                icon: "success",
-            });
-            fetchData();
-        });
-    };
+        })
+            .then((resp) => {
+                console.log(resp["data"]);
+                console.log("success log");
+                Swal.fire({
+                    title: "Success",
+                    text: "Product Deleted Successfully",
+                    icon: "success",
+                });
+                fetchData();
+            }).catch ((error) => {
+                console.log(error);
+                Swal.fire({
+                    title: "Error",
+                    text: "Product Deletion Failed",
+                    icon: "error",
+                });
+            }
+            );
+
+    }
+
+
+
+            
+
 
     const [product, setProduct] = useState([]);
 
