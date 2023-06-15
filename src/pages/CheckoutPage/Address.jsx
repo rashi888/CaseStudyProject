de0efe4017c464aba714ae6a92b6dd5f7f500bc9
rashi <rashidixit129@gmail.com>
2023-06-16 01:27:13 +0530

    addresses

--- src/pages/CheckoutPage/Address.jsx
+++ src/pages/CheckoutPage/Address.jsx
@@ -576,7 +576,7 @@ function Address() {
                       </Col>
                     </Row>
                     <FormGroup>
-                      <Label for="exampleAddress">Address</Label>
+                      <Label for="exampleAddress">Flat, House no., Building, Company, Apartment</Label>
                       <Input
                         id="exampleAddress"
                         name="addressLine1"
@@ -585,12 +585,12 @@ function Address() {
                       />
                     </FormGroup>
                     <FormGroup>
-                      <Label for="exampleAddress2">Address 2</Label>
+                      <Label for="exampleAddress2">Area, Street, Sector</Label>
                       <Input
                         id="exampleAddress2"
                         required
                         name="addressLine2"
-                        placeholder="Apartment, studio, or floor"
+                        placeholder="Area, Street, Sector"
                       />
                     </FormGroup>
                     <Row>
